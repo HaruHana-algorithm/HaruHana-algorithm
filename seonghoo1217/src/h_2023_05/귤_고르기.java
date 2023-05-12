470ccdd350cfbc08d7877b76f1c9fc18b4a68e31
seonghoo1217 <seonghoo1217@naver.com>
2023-05-12 22:48:02 +0900

    20230512

---
+++ seonghoo1217/src/h_2023_05/귤_고르기.java
@@ -0,0 +1,34 @@
+package h_2023_05;
+
+import java.util.*;
+
+public class 귤_고르기 {
+	static HashMap<Integer, Integer> hashMap=new HashMap<>();
+
+	public int solution(int k, int[] tangerine) {
+		for (int x:tangerine){
+			hashMap.put(x,hashMap.getOrDefault(x,0)+1);
+		}
+		List<Integer> keylist = new ArrayList<>(hashMap.keySet());
+		Collections.sort(keylist, new customComparator());
+		int answer = 0;
+
+		// 정렬된 key리스트에서 값을 하나씩 가져와 k에 빼줌
+		for (Integer e : keylist) {
+			if (k <= 0)
+				break;
+			answer++;
+			k -= hashMap.get(e);
+		}
+		return answer;
+	}
+
+	public class customComparator implements Comparator<Integer> {
+
+		@Override
+		public int compare(Integer o1, Integer o2) {
+			return hashMap.get(o2).compareTo(hashMap.get(o1));
+		}
+	}
+
+}
