2767ca2ede5e68de5c9c65bd9daf95e6110c35e7
seonghoo1217 <seonghoo1217@naver.com>
2023-06-07 18:56:23 +0900

    20230607

---
+++ seonghoo1217/src/h_2023_06/게임_맵_최단사전.java
@@ -0,0 +1,44 @@
+package h_2023_06;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 게임_맵_최단사전 {
+	static int answer = 0;
+	static int [] dx={1,0,-1,0};
+	static int [] dy={0,1,0,-1};
+	public int solution(int[][] maps) {
+		int [][] visited=new int[maps.length][maps[0].length];
+		bfs(maps,visited);
+		answer=visited[maps.length-1][maps[0].length];
+
+		if (answer==0) answer=-1;
+		return answer;
+	}
+
+	public void bfs(int maps[][],int [][] visited){
+		int x=0;
+		int y=0;
+		visited[x][y]=1;
+		Queue<int[]> queue=new LinkedList<>();
+		queue.add(new int[]{x,y});
+
+		while (!queue.isEmpty()){
+			int[] current = queue.remove();
+			int currentX = current[0];
+			int currentY = current[1];
+
+			for (int i=0;i<4;i++){
+				int nowX = currentX + dx[i];
+				int nowY = currentY + dy[i];
+
+				if (nowX<0||nowX>maps.length-1||nowY<0||nowY>maps[0].length) continue;
+
+				if (visited[nowX][nowY]==0&&maps[nowX][nowY]==1){
+					visited[nowX][nowY]=visited[currentX][currentY]+1;
+					queue.add(new int[]{nowX,nowY});
+				}
+			}
+		}
+	}
+}
