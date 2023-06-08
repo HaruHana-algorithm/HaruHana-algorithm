a31c3804a90b6bc5604b4c4f62252911caf6d8ca
seonghoo1217 <seonghoo1217@naver.com>
2023-06-08 17:40:46 +0900

    20230608

---
+++ seonghoo1217/src/h_2023_06/더_맵게.java
@@ -0,0 +1,30 @@
+package h_2023_06;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class 더_맵게 {
+	public int solution(int[] scoville, int K) {
+		PriorityQueue<Integer> pq=new PriorityQueue<>();
+		int answer=0;
+		for (int s:scoville){
+			pq.add(s);
+		}
+
+		while (pq.peek()<K){
+			if (pq.size()==1){
+				return -1;
+			}
+			pq.add(pq.poll()+(pq.poll()*2));
+			answer++;
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		int [] scovile={1, 2, 3, 9, 10, 12};
+		int k=7;
+		더_맵게 so=new 더_맵게();
+		so.solution(scovile,k);
+	}
+}
