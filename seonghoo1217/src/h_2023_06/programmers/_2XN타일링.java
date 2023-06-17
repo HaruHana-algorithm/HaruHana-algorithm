81fa0f736e3c652fa71d6016c520a2b53a6a28b2
seonghoo1217 <seonghoo1217@naver.com>
2023-06-17 19:23:44 +0900

    20230617

---
+++ seonghoo1217/src/h_2023_06/programmers/_2XN타일링.java
@@ -0,0 +1,18 @@
+package h_2023_06.programmers;
+
+public class _2XN타일링 {
+	public int solution(int n) {
+		int answer = 0;
+
+		int[] arr = new int[n];
+		arr[0] = 1;
+		arr[1] = 2;
+
+		for (int i = 2; i < n; i++) {
+			int num = arr[i - 1] + arr[i - 2];
+			arr[i] = num % 1000000007;
+		}
+
+		return arr[n-1];
+	}
+}
