c79637d3d68a3eb4a6c2ef7e92e68151901b7c62
seonghoo1217 <seonghoo1217@naver.com>
2023-09-19 22:39:58 +0900

    ATM

---
+++ seonghoo1217/src/university/ATM.java
@@ -0,0 +1,22 @@
+package university;
+
+import java.util.*;
+
+public class ATM{
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int num = scanner.nextInt();
+        int[] arr=new int[num];
+        for (int i=0;i<num;i++){
+            arr[i] = scanner.nextInt();
+        }
+        Arrays.sort(arr);
+        int sum=arr[0];
+        for (int i=1;i<arr.length;i++){
+            arr[i]=arr[i]+arr[i-1];
+            sum+=arr[i];
+        }
+        System.out.print(sum);
+        scanner.close();
+    }
+}
